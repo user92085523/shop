636e47339cbaf1a6fdb810e21a510f99eb229ef6
test <test@example.com>
2025-04-19 05:11:41 +0900

    test

--- app/MyHelper/Class/QueryBuilder.php
+++ app/MyHelper/Class/QueryBuilder.php
@@ -2,24 +2,51 @@
 
 namespace App\MyHelper\Class;
 
+use App\Models\User;
+
 class QueryBuilder
 {
     public static function BuildQuery($query_ingredients)
     {
         $query = $query_ingredients['class']::query();
 
-        foreach ($query_ingredients['inputs'] as $unused => $input) {
-            if (! (isset($input['column']) && isset($input['method']) && isset($input['operator']) && isset($input['value']) && $input['value'] !== '')) {
-                continue;
-            }
+        // dump($query_ingredients);
 
-            if ($input['operator'] === 'like') {
-                $query = $query->{$input['method']}($input['column'], $input['operator'],"%{$input['value']}%");
-            } elseif ($input['operator'] === '=') {
-                $query = $query->{$input['method']}($input['column'], $input['operator'], $input['value']);
+        foreach ($query_ingredients['inputs'] as $unused => $input) {
+            $column = $input['column'];
+
+            for ($i=0; $i < count($input['method']); $i++) { 
+                $method = $input['method'][$i];
+
+                if ($method === '') continue;
+
+                if (in_array($method, ['onlyTrashed', 'withTrashed'])) {
+                    $query = self::onlyMethod($query, $method);
+                } elseif (in_array($method, ['where','whereDate'])) {
+                    $operator = $input['operator'][$i];
+                    $value = $input['value'][$i];
+
+                    if ($operator === '' || $value === '') continue;
+
+                    $query = self::unnamed($query, $column, $method, $operator, $value);
+                }
             }
         }
 
         return $query;
+    }
+
+    private static function onlyMethod(&$query, $method)
+    {
+        return $query->$method();
+    }
+
+    private static function unnamed(&$query, $column, $method, $operator, $value)
+    {
+        if ($operator === 'like') {
+            $value = "%$value%";
+        }
+
+        return $query->$method($column, $operator, $value);
     }
 }
