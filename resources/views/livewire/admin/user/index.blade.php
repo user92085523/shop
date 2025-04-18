9aa8673b5d787c14680eb42c09e080194572c88d
test <test@example.com>
2025-04-19 05:44:24 +0900

    test

--- resources/views/livewire/admin/user/index.blade.php
+++ resources/views/livewire/admin/user/index.blade.php
@@ -11,13 +11,13 @@
             @foreach ($row as $option)
                 {{-- <{{ $loop->parent->parent->index }}.{{ $loop->parent->index }}.{{ $loop->index }}> --}}
                 @if ($option['html_tag'] === 'select')
-                    <select name="" id="" wire:model.live="csf_search_forms.{{ $loop->parent->parent->index }}.{{ $loop->parent->index }}.{{ $loop->index }}.obj_idx" wire:key="csf_search_forms.{{ $loop->parent->parent->index }}.{{ $loop->parent->index }}.{{ $loop->index }}.obj_idx">
+                    <select name="" id="" wire:model.change="csf_search_forms.{{ $loop->parent->parent->index }}.{{ $loop->parent->index }}.{{ $loop->index }}.obj_idx" wire:key="csf_search_forms.{{ $loop->parent->parent->index }}.{{ $loop->parent->index }}.{{ $loop->index }}.obj_idx">
                         @foreach ($option['objs'] as $item)
                             <option value="{{ $loop->index }}">{{ $item['label'] }}</option>
                         @endforeach
                     </select>
                 @elseif ($option['html_tag'] === 'input')
-                    <input type="text" wire:model.live="csf_search_forms.{{ $loop->parent->parent->index }}.{{ $loop->parent->index }}.{{ $loop->index }}.objs.{{ $csf_search_forms[$loop->parent->parent->index][$loop->parent->index][$loop->index]['obj_idx'] }}.data.value" placeholder="{{ $option['objs'][$option['obj_idx']]['label'] }}">
+                    <input type="text" wire:model.change="csf_search_forms.{{ $loop->parent->parent->index }}.{{ $loop->parent->index }}.{{ $loop->index }}.objs.{{ $csf_search_forms[$loop->parent->parent->index][$loop->parent->index][$loop->index]['obj_idx'] }}.data.value" placeholder="{{ $option['objs'][$option['obj_idx']]['label'] }}">
                 @endif
                 {{-- @dump($csf_search_forms[$loop->parent->parent->index][$loop->parent->index][$loop->index]) --}}
             @endforeach
@@ -28,7 +28,7 @@
         @endif
         <br>
         @if ($loop->last && count($csf_search_forms) < $csf_max_search_form_cnt)
-            <button wire:click="csf_AddSearchForm">さらに条件を追加する</button>
+            <button wire:click="csf_AddSearchForm">検索カラムの追加</button>
         @endif
     @endforeach
 
